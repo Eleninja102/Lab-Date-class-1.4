f8bf193a5d2318b8137c1920b9afad2bd92fb26b
Coleton Watt <electricninja102@gmail.com>
2022-01-15 22:25:36 -0700

    Initial Commit

---
+++ Lab: Date class 1.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab: Date class 1.4
+//
+//  Created by Coleton Watt on 1/15/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
